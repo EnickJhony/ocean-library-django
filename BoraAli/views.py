d3895d297effd779fc705d67f34bc0a09d472bfa
Enick Jhony <enick.jhony@gmail.com>
2022-10-04 21:59:42 -0300

    finalizando aula dia 04/10

--- BoraAli/views.py
+++ BoraAli/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from BoraAli.models import Autor, Livro
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'BoraAli/home.html')
+
+
+def autor(request):
+    autores = Autor.objects.all()
+
+    context
+
+
--- projeto01/urls.py
+++ projeto01/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('autor', views.autor, name='autor')
 ]
